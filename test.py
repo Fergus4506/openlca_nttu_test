31d971b6363433a1ed20f779519bd32a782fcdf8
Fergus4506 <115529614+Fergus4506@users.noreply.github.com>
2025-12-18 16:21:10 +0800

    [upload] 測試用python改為兩種方式接測試

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 # call_flask.py
 import requests
 
-url = "http://localhost:5001/calculate"  # 確認 Flask 正在跑
+#第一部分 以距離為碳排計算標準
+url = "http://localhost:5001/calculate/Co2BYTKM"  # 確認 Flask 正在跑
 payload = {
     # 若你已在 Flask 把 Product System 與 Method 固定，這裡就只送四個參數
     "distance": 164,
@@ -12,5 +13,21 @@ payload = {
 }
 
 res = requests.post(url, json=payload)
+print("HTTP status:", res.status_code)
+print(res.json())
+
+# 第二部分 以油耗為碳排計算標準
+url_oil = "http://localhost:5001/calculate/Co2BYOilKM"  # 確認 Flask 正在跑
+payload = {
+    # 若你已在 Flask 把 Product System 與 Method 固定，這裡就只送四個參數
+    "distance": 164,
+    "factor": 3.32,
+    "load": 12.6,
+    "amount": 0.151,
+    "oilUse": 58.57
+    # 若沒有固定，請加入 product_system_id 與 impact_method_id
+}
+
+res = requests.post(url_oil, json=payload)
 print("HTTP status:", res.status_code)
 print(res.json())
